4c07968c33c9d8fbb7044475d29220656d393dae
Zarcam <zarcam.t@gmail.com>
2024-02-01 18:15:37 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Biblioteca Comics Simple", 
+    'summary': "Gestionar comics :) (Version simple)", 
+    'description': """
+        Gestor de bibliotecas (Version Simple)
+        ==============
+    """,  
+
+    'application': True,
+    'author': "Sergi García",
+    'website': "http://apuntesfpinformatica.es",
+    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['base'],
+
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/biblioteca_comic.xml'
+    ],
+}
